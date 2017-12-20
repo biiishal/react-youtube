54704c14eb89fa8b52177db1c01246ac315535d3
Bishal Shrestha <bishalshrestha33@gmail.com>
2017-12-20 21:42:58 +0545

    Add gh-pages for deployment

--- src/video_detail.js
+++ src/video_detail.js
@@ -8,7 +8,7 @@ const VideoDetail = ({video}) => {
   const url = "https://www.youtube.com/embed/" + video.id;
   return(
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe src={url} className="embed-responsive-item" frameBorder="0" allowFullScreen></iframe>
+        <iframe src={url} className="embed-responsive-item" frameBorder="0" title={video.title} allowFullScreen></iframe>
       </div>
     )
 }
